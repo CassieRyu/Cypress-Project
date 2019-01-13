f43cfd127e4e9d27bb109462e165168f2b879352
shanliu <793399678@qq.com>
2019-01-13 12:51:48 +0800

    combine mochawesome reporter

--- testRunner.sh
+++ testRunner.sh
@@ -5,20 +5,22 @@ SUITE="$2"
 SPEC="**/**.js"
 
 if [ "${SUITE}" = "visual" ] ; then
-    rm -r cypress/snapshots/visualTesting/**/__diff_output__
+    rm -r cypress/snapshots/visual/**/__diff_output__
     SPEC="cypress/integration/visual/*.js"
-fi
+else
 
 if [ "${SUITE}" = "functional" ] ; then
     SPEC="cypress/integration/functional/*.js"
 
 else
-     rm -r cypress/snapshots/visualTesting/**/__diff_output__
+     rm -r cypress/snapshots/visual/**/__diff_output__
      SPEC="cypress/integration/**/**.js"
+fi
 fi
 
 echo ">>>: ENV:${ENV}, SUITE:${SUITE}, SPEC:${SPEC}"
 ./node_modules/.bin/cypress run --env configFile=${ENV} --spec "${SPEC}"
+
 result=$?
-node scripts/test.js
+ node scripts/mergeReports.js
 exit $result
